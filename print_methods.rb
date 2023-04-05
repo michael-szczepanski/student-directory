0ee402841ad67ed77b6976e7361d738d888094a4
BetterAshThanDust <h4y473@gmail.com>
2023-04-05 13:51:11 +0100

    io_methods.rb fixed

--- print_methods.rb
+++ print_methods.rb
@@ -1,86 +1,92 @@
 def print_all_by_cohort
   
-    print_header
-    cohorts = {}
+  print_header
+  cohorts = {}
     
-    @students.each do |student|
-      cohort = student[:cohort]
+  @students.each do |student|
+    cohort = student[:cohort]
       
-      if !cohorts.include?(cohort)
-        cohorts[cohort.to_sym] = []
-      end
+    if !cohorts.include?(cohort)
+      cohorts[cohort.to_sym] = []
+    end
       
-      cohorts[cohort] << student[:name]
+    cohorts[cohort] << student[:name]
+  end
+    
+  cohorts.each do |cohort, students|
+    puts "#{cohort}: #{students}"
+  end
+    
+end
+  
+def print_by_cohort(cohort)
+    
+  counter = 1
+  @students.each do |student|
+    if student[:cohort] == cohort.to_sym
+      puts "#{counter}. #{student[:name]}"
+      counter += 1
     end
+  end
     
-    cohorts.each do |cohort, students|
-      puts "#{cohort}: #{students}"
+end
+  
+def print_footer
+  footer = "Overall, we have #{@students.count} great student"
+  footer << "s" if @students.length > 1
+    
+  puts footer
+    
+end
+  
+def print_header
+  puts "The students of Villains Academy\n---------------"
+end
+  
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list of students"
+  puts "4. Load the list of students"
+  puts "9. Exit"
+end
+  
+def print_shorter_than(number)
+    
+  puts "Student names shorter than #{number} characters:"
+  counter = 0
+  @students.each do |student|
+    if student[:name].length < number
+      counter += 1
+      puts "#{counter}. #{student[:name]}"
     end
-    
   end
-  
-  def print_by_cohort(cohort)
     
-    counter = 1
-    @students.each do |student|
-      if student[:cohort] == cohort.to_sym
-        puts "#{counter}. #{student[:name]}"
-        counter += 1
-      end
+end
+  
+def print_starting_with(letter)
+    
+  puts "Student names starting with letter #{letter}:"
+  counter = 0
+  @students.each do |student|
+    if student[:name][0] == letter
+      counter += 1
+      puts "#{counter}. #{student[:name]}"
     end
-    
   end
+    
+end
   
-  def print_footer
-    footer = "Overall, we have #{@students.count} great student"
-    footer << "s" if @students.length > 1
+def print_students_list
     
-    puts footer
-    
+  @students.each_with_index do |student, student_no|
+    puts "#{student_no + 1}. #{student[:name]} (Cohort: #{student[:cohort]})"
   end
-  
-  def print_header
-    puts "The students of Villains Academy\n---------------"
-  end
-  
-  def print_menu
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "3. Save the list of students"
-    puts "4. Load the list of students"
-    puts "9. Exit"
-  end
-  
-  def print_shorter_than(number)
     
-    puts "Student names shorter than #{number} characters:"
-    counter = 0
-    @students.each do |student|
-      if student[:name].length < number
-        counter += 1
-        puts "#{counter}. #{student[:name]}"
-      end
-    end
-    
-  end
-  
-  def print_starting_with(letter)
-    
-    puts "Student names starting with letter #{letter}:"
-    counter = 0
-    @students.each do |student|
-      if student[:name][0] == letter
-        counter += 1
-        puts "#{counter}. #{student[:name]}"
-      end
-    end
-    
-  end
-  
-  def print_students_list
-    
-    @students.each_with_index do |student, student_no|
-      puts "#{student_no + 1}. #{student[:name]} (Cohort: #{student[:cohort]})"
-    end
-    
-  end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
