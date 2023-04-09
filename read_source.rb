e5a50420fd29d0956d904fde1a167736cabdde4e
Michael Szczepanski <m.szczepanski90@gmail.com>
2023-04-09 10:19:34 +0100

    read_source updated

--- read_source.rb
+++ read_source.rb
@@ -1,1 +1,22 @@
-puts __FILE__
+@students = []
+
+@cohorts = [
+  "january",
+  "february",
+  "march",
+  "april",
+  "may",
+  "june",
+  "july",
+  "august",
+  "september",
+  "october",
+  "november",
+  "december"
+]
+
+def print_cohorts
+  puts @cohorts.to_s
+end
+
+puts File.read(__FILE__)
